69561c53e062ac9d1be3fc3a4f75c2ed61f83b1f
habioye <78454514+habioye@users.noreply.github.com>
2024-06-12 13:19:57 -0400

    Created A repository connection

---
+++ src/main/java/CustomerManagmentSystem/Repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package CustomerManagmentSystem.Repository;
+
+import CustomerManagmentSystem.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
