c1f4d0370a194d2e3501dc38caf5a68393491904
habioye <78454514+habioye@users.noreply.github.com>
2024-06-12 11:23:38 -0400

    Initial commit

---
+++ src/main/java/CustomerManagmentSystem/CustomerManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package CustomerManagmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CustomerManagmentSystem/CustomerManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package CustomerManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
